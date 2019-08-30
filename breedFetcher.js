7db35c18c319b932f2b7d0c1c4b70c464f727a50
vagrant <Kennethzhang374@gmail.com>
2019-08-30 00:48:19 +0000

    refactoredd into index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
+  const request = require('request');
 
-const request = require('request');
-const name = process.argv.slice(2).toString();
-request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
-  if (error) {
-    console.log(`There is an error with your URL\n`, error);
-    return;
-  } if (body.length === 2) {
-    console.log("The Breed Name Is Not Found");
-    return;
-  }
-  const data = JSON.parse(body);//convert string to object/an array;
-  console.log(data[0]['description']);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(`There is an error with your URL\n`, error);
+      return;
+    } if (body.length === 2) {
+      callback("The Breed Name Is Not Found", null);
+      return;
+    }
+    const data = JSON.parse(body);//convert string to object/an array;
+    callback(null,data[0]['description']);
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
 
